17784037d767fb0130134df3033e6c48349ddf40
code-playmaker <s.ueno1229@gmail.com>
2020-05-11 20:27:52 +0900

    configuration_routing and messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,14 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.all
   end
+
+  def create
+    Message.create(message_params)
+  end
+
+  private
+    def messages_params
+      params.require(:message).permit(:content, :image)
+    end
 end
